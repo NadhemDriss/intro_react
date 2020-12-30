ce9ee97a0e5227383abf5a2037e27eeb237a5a8b
NadhemDriss <64647030+NadhemDriss@users.noreply.github.com>
2020-12-30 14:30:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Acceuil from './component/Acceuil';
+
+
+function App() {
+  return (
+    <div>
+     <Acceuil/>
+    </div>
+  );
+}
+
+export default App;
